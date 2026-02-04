3663a13f144de61f0a1fc1228d15a48703b97cc9
naman2398 <naman230598@gmail.com>
2026-02-03 19:08:05 -0500

    handled import errors to accomaodate password login

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -1,10 +1,15 @@
 """Authentication Module"""
 
 from .msal_auth import get_access_token
-from .user_auth import get_authorized_users, check_user_authorization
+from .user_auth import get_app_password, check_password_authorization
+
+# Commented out - requires SharePoint access for App_Access_List.xlsx
+# from .user_auth import get_authorized_users, check_user_authorization
 
 __all__ = [
     "get_access_token",
-    "get_authorized_users",
-    "check_user_authorization"
+    "get_app_password",
+    "check_password_authorization",
+    # "get_authorized_users",
+    # "check_user_authorization"
 ]
